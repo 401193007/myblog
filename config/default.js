ac44e845f77c8d172108bce345e0077a00e6eae8
dh527200 <401193007@qq.com>
2017-08-15 23:42:53 +0800

    提交配置注释

--- config/default.js
+++ config/default.js
@@ -1,9 +1,11 @@
 module.exports = {
   port: 3000,
+  //express-session 的配置信息
   session: {
     secret: 'myblog',
     key: 'myblog',
     maxAge: 2592000000
   },
+  // mongodb 的地址  myblog 为 db 名B
   mongodb: 'mongodb://localhost:27017/myblog'
 };
