22c4187639eeeda8a05da71f00f9f62b0e46c4d0
自由人皇 <401193007@qq.com>
2017-08-16 15:09:32 +0800

    修改入口文件

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var express = require('express');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
 
-
-var routes = require('./routes');
-var pkg = require('./package');
+var config = require("./config/default.js")
+var routes = require('./routes/index.js');
+var pkg = require('./package.json');
 
 var app = express(); //启动express
 
@@ -13,6 +15,9 @@ app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
 
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(session({
 	name : config.session.key,   //设置cookie中保存session id的字段名称
 	secret : config.session.secret,  //通过设置secret来计算hash值并放在cookie中，使产生的signedCookie防篡改
@@ -20,8 +25,28 @@ app.use(session({
 	saveUninitialized : false,// 设置为 false，强制创建一个 session，即使用户未登录
 	cookie : {
 		maxAge : config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
-	}
+	},
+	store: new MongoStore({// 将 session 存储到 mongodb
+		url: config.mongodb// mongodb 地址
+	})	
 }));	
+
+// flash 中间件，用来显示通知
+app.use(flash());
+
+// 设置模板全局常量
+app.locals.blog = {
+	title: pkg.name,
+	description: pkg.description
+};
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+	res.locals.user = req.session.user;
+	res.locals.success = req.flash('success').toString();
+	res.locals.error = req.flash('error').toString();
+	next();
+});
 
 // 路由
 routes(app);
