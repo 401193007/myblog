16457e5e0c8ac86c0000a59e6f416817af00e0e8
自由人皇 <401193007@qq.com>
2017-08-17 10:44:48 +0800

    signin and signout

--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,14 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, function(req, res, next) {
-	res.send(req.flash());
+
+	//清空session中的用户信息
+	req.session.user = null;
+	//登出成功提示
+	req.flash("success","登出成功");
+	//登出成功后的跳页
+	res.redirect("/posts");
+	
 });
 
 module.exports = router;
